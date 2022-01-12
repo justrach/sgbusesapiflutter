c28792b4a06bc32de55f4a25fe688ffbefb10d80
Rach <54503978+justrach@users.noreply.github.com>
2022-01-12 17:31:58 +0800

    added_models as a source folder

---
+++ lib/sg_bus_api.dart
@@ -0,0 +1,28 @@
+library bus_api;
+
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:sg_bus_api/src/bus_model.dart';
+
+
+class SGBusAPI{
+  //INPUT: Bus Stop 
+  //returns a Future With a List of Services that the BUS Stop operates with
+ static Future<List<Services>> getBusStopInformation(String busStopID) async {
+
+   Dio dio = Dio();
+    final response = await dio.get(
+ 
+            "https://www.rach.codes/api/bustiming?id==$busStopID",
+        );
+
+
+    var x = json
+        .decode(response.data)["Services"]
+        .map((data) => Services.fromJson(data))
+        .toList();
+    List<Services> streetsList = List<Services>.from(x);
+    return streetsList;
+  }
+
+}
